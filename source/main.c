cbf43fa3f147cb1465ffa84afcf7bf282305aa70
Jack Nelson <76402596+br3akthescr1pt@users.noreply.github.com>
2026-07-01 10:01:57 -0500

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,6 +1,7 @@
 #include "hv_defeat_0304.h"
 #include "hv_defeat_0506.h"
 #include "hv_defeat_0607.h"
+
 #include "loader.h"
 #include "prepare_resume.h"
 #include "utils.h"
@@ -20,27 +21,41 @@ int main(void) {
 
   void *shellcode_kernel;
   size_t shellcode_kernel_len;
+
   if (prepare_resume(&shellcode_kernel, &shellcode_kernel_len)) {
     notify("Something went wrong while preparing resume.\n");
     return -1;
   }
 
-  if ((0x0300 <= fw) && (fw < 0x0500)) {
-    if (hv_defeat_0304(shellcode_kernel, shellcode_kernel_len))
+  switch (fw) {
+
+    case 0x0300 ... 0x04ff:
+      if (hv_defeat_0304(shellcode_kernel, shellcode_kernel_len))
+        goto err;
+      break;
+
+    case 0x0500 ... 0x064f:
+      if (hv_defeat_0506(shellcode_kernel, shellcode_kernel_len))
+        goto err;
+      break;
+
+    case 0x0650 ... 0x07ff:
+      if (hv_defeat_0607(shellcode_kernel, shellcode_kernel_len))
+        goto err;
+      break;
+
+    case 0x0940:
+      if (hv_defeat_0607(shellcode_kernel, shellcode_kernel_len))
+        goto err;
+      break;
+
+    default:
       goto err;
-  } else if ((0x0500 <= fw) && (fw < 0x0650)) {
-    if (hv_defeat_0506(shellcode_kernel, shellcode_kernel_len))
-      goto err;
-  } else if ((0x0650 <= fw) && (fw < 0x0800)) {
-    if (hv_defeat_0607(shellcode_kernel, shellcode_kernel_len))
-      goto err;
-  } else {
-    goto err;
   }
 
-  notify("Finished preparation. Going to rest mode in 5 seconds.\nPlease wait "
-         "for the orange light to stop "
-         "blinking and then wakeup to Linux :)\n");
+  notify("Finished preparation. Going to rest mode in 5 seconds.\n"
+         "Please wait for the orange light to stop blinking and then wakeup "
+         "to Linux :)\n");
 
   sleep(5);
   enter_rest_mode();
@@ -52,7 +67,7 @@ int main(void) {
   return 0;
 
 err:
-  notify("Something went wrong while defeating Hypervisor.\nPlease make sure "
-         "your fw is supported.");
+  notify("Something went wrong while defeating Hypervisor.\n"
+         "Please make sure your fw is supported.");
   return -1;
 }
